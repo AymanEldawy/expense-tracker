f05681f394a04fe47e3ae4a008b925f72de33e42
= <=>
2022-02-19 23:13:26 -0800

    Updates

---
+++ precache-manifest.4de9cc8b83bca4d999377d5d09373e24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f79f9a632ce7637861c",
+    "url": "/expense-tracker/static/css/main.b8e23c98.chunk.css"
+  },
+  {
+    "revision": "2f79f9a632ce7637861c",
+    "url": "/expense-tracker/static/js/main.bb157fbe.chunk.js"
+  },
+  {
+    "revision": "5baca6083ea1223d23cf",
+    "url": "/expense-tracker/static/js/runtime~main.ae18d0c9.js"
+  },
+  {
+    "revision": "04475f8c591d76f7b9bb",
+    "url": "/expense-tracker/static/js/2.7b6d8154.chunk.js"
+  },
+  {
+    "revision": "5c9988e8330afb6074529a2c02b79244",
+    "url": "/expense-tracker/index.html"
+  }
+];
